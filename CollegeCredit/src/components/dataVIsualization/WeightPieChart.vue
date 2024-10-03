45bcd8a9969466934fc534383aff82f6fc8dc6ce
crouchbindcommit <mmckin22@vols.utk.edu>
2024-10-03 12:21:40 -0400

    withChart

---
+++ CollegeCredit/src/components/dataVIsualization/WeightPieChart.vue
@@ -0,0 +1,21 @@
+<template>
+
+
+</template>
+
+
+<script lang="ts">
+
+
+</script>
+
+weights = [
+    num paymentHistory;
+    num amountOwe;
+    num historyLength;
+    num creditMix; 
+    num newCredit;  
+]; 
+
+
+<style scoped></style>
--- CollegeCredit/src/components/userForm.vue
+++ CollegeCredit/src/components/userForm.vue
@@ -1,12 +1,35 @@
 <template>
 
+    <!-- Log In if no Suspicious Activtiy is suspected-->
+    <template v-if="suspicousActivity == false">
+        <v-text-field
+            v-model="username"
+            :counter="12"
+            :rules="usernameRules"
+            label="username"
+            hide-details
+            required
+        >
+        </v-text-field>
+
+    </template>
+
 </template>
-  
-  <script setup lang="ts">
 
-  </script>
-  
-  <style scoped lang="sass">
 
-  </style>
+<script setup lang="ts">
+
+    var email = '';
+    var password = ''; 
+    var suspicousActivity = false;
+
+    user:
+    
+
+</script>
+  
+  
+
+
+<style scoped lang="sass"></style>
   
